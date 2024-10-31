7fb257d2e08e0ea08c55405aed732ab5001354e2
VI-Suji <sujithvi08@gmail.com>
2024-10-31 07:43:50 +0530

    final code for bank

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -9,6 +9,20 @@ impl Account {
     fn new(id: u32, holder: String) -> Self {
         Account {balance: 0, id, holder}
     }
+
+    fn deposit(&mut self, amount: i32) -> i32{
+        self.balance += amount;
+        self.balance
+    }
+
+    fn withdraw(&mut self, amount: i32) -> i32{
+        self.balance -= amount;
+        self.balance
+    }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance of {}",self.holder,self.balance)
+    }
 }
 
 #[derive(Debug)]
@@ -20,20 +34,42 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account);
+    }
 
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
+
+    fn summary(&self) -> Vec<String> {
+        self.accounts.iter().map(|account|account.summary()).collect::<Vec<String>>()
+    }
 }
 
-fn print_acc(account: &Account){
-    println!("{:#?}",account);
-}
 
 fn main() {
-    let bank = Bank::new();
-    println!("Bank is {:#?}",bank);
-    let account = Account::new(1, String::from("me"));
-    let mut account_ref = &account;
-    let account_ref_2 = &account;
-    print_acc(account_ref);
-    print_acc(account_ref_2);
-    print_acc(account_ref);
+    let mut bank = Bank::new();
+    let mut account = Account::new(1,String::from("new_user"));
+    let mut account1 = Account::new(2,String::from("new_user_1"));
+    let mut account2 = Account::new(3,String::from("new_user_2"));
+
+    // bank.add_account(account);
+    // println!("{:#?}", bank);
+
+    account.deposit(500);
+    account.withdraw(250);
+    account1.deposit(1000);
+    account1.withdraw(250);
+    account2.deposit(100);
+    account2.withdraw(25);
+
+    println!("{:#?}", account.summary());
+    bank.add_account(account);
+    bank.add_account(account1);
+    bank.add_account(account2);
+    println!("{:#?}", bank);
+
+    println!("{:#?}", bank.summary());
+    println!("{}", bank.total_balance());
 }
